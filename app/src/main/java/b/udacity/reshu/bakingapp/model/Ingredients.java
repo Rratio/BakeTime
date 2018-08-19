fb0c6b1d810bef50e8b2d52befcae6e806925517
reshu <reshu1207@gmail.com>
2018-08-19 15:45:46 +0530

    Third commit

--- app/src/main/java/b/udacity/reshu/bakingapp/model/Ingredients.java
+++ app/src/main/java/b/udacity/reshu/bakingapp/model/Ingredients.java
@@ -1,5 +1,8 @@
 package b.udacity.reshu.bakingapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -7,7 +10,9 @@ import com.google.gson.annotations.SerializedName;
  * Created by lenovo-pc on 8/8/2018.
  */
 
-public class Ingredients {
+public class Ingredients implements Parcelable {
+
+
 
     @SerializedName("quantity")
     @Expose
@@ -24,6 +29,28 @@ public class Ingredients {
         this.measure = measure;
         this.ingredient = ingredient;
     }
+
+    protected Ingredients(Parcel in) {
+        if (in.readByte() == 0) {
+            quantity = null;
+        } else {
+            quantity = in.readFloat();
+        }
+        measure = in.readString();
+        ingredient = in.readString();
+    }
+
+    public static final Creator<Ingredients> CREATOR = new Creator<Ingredients>() {
+        @Override
+        public Ingredients createFromParcel(Parcel in) {
+            return new Ingredients(in);
+        }
+
+        @Override
+        public Ingredients[] newArray(int size) {
+            return new Ingredients[size];
+        }
+    };
 
     public Float getQuantity() {
         return quantity;
@@ -47,5 +74,22 @@ public class Ingredients {
 
     public void setIngredient(String ingredient) {
         this.ingredient = ingredient;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        if (quantity == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeFloat(quantity);
+        }
+        parcel.writeString(measure);
+        parcel.writeString(ingredient);
     }
 }
